8fab3e6e3e9c8691ea89d83a28988a4213ece74a
Jordan Carlile <jordan@Jordans-MacBook-Air.local>
2023-09-01 17:47:46 -0500

    testgpt route is working

--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -2,11 +2,17 @@
 
 const express = require("express");
 // const sendWhatsappMsg = require("../utils/sendWhatsappMsg");
+const { APIcall, singleGPTCall } = require('../utils/getTextFromChatGPT');
+
 
 const router = express.Router();
 
 router.route("/msg").post(async (req, res) => {
   res.send({ message: "success" });
 });
+
+router.route("/testgpt").post(async(req, res) => {
+  res.send({ result: await singleGPTCall(req.body.prompt) });
+})
 
 module.exports = router;
--- utils/getTextFromChatGPT.js
+++ utils/getTextFromChatGPT.js
@@ -1,6 +1,5 @@
 // https://www.geeksforgeeks.org/how-to-use-chatgpt-api-in-nodejs/
 const { Configuration, OpenAIApi } = require("openai");
-const readlineSync = require("readline-sync");
 require("dotenv").config();
 
 let APIcall = async () => {
@@ -9,36 +8,56 @@ const newConfig = new Configuration({
 });
 const openai = new OpenAIApi(newConfig);
 	
-const chatHistory = [];
+const chatHistory = []; // todo look up based on phone number or something else... DB?
+const user_input = readlineSync.question("Enter your input: "); // todo get from parameter of function
+const messageList = chatHistory.map(([input_text, completion_text]) => ({
+role: "user" === input_text ? "ChatGPT" : "user",
+content: input_text
+}));
+messageList.push({ role: "user", content: user_input });
 
-do {
-	const user_input = readlineSync.question("Enter your input: ");
-	const messageList = chatHistory.map(([input_text, completion_text]) => ({
-	role: "user" === input_text ? "ChatGPT" : "user",
-	content: input_text
-	}));
-	messageList.push({ role: "user", content: user_input });
+try {
+const GPTOutput = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    prompt: user_input,
+});
 
-	try {
-	const GPTOutput = await openai.createChatCompletion({
-		model: "gpt-3.5-turbo",
-		messages: messageList,
-	});
+const output_text = GPTOutput.data.choices[0].message.content;
+console.log(output_text);
 
-	const output_text = GPTOutput.data.choices[0].message.content;
-	console.log(output_text);
-
-	chatHistory.push([user_input, output_text]);
-	} catch (err) {
-	if (err.response) {
-		console.log(err.response.status);
-		console.log(err.response.data);
-	} else {
-		console.log(err.message);
-	}
-	}
-} while (readlineSync.question("\nYou Want more Results? (Y/N)").toUpperCase() === "Y");
+chatHistory.push([user_input, output_text]);
+} catch (err) {
+if (err.response) {
+    console.log(err.response.status);
+    console.log(err.response.data);
+} else {
+    console.log(err.message);
+}
+}
 };
 
-APIcall();
-module.exports = APIcall;
+let singleGPTCall = async(user_input) => {
+    console.log("user input is: ", user_input)
+    const newConfig = new Configuration({
+        apiKey: process.env.OPENAI_SECRET_KEY
+    });
+    const openai = new OpenAIApi(newConfig);
+    try {
+        const GPTOutput = await openai.createCompletion({
+            model: "text-davinci-002",
+            prompt: user_input,
+            max_tokens: 100
+        });
+        console.log(GPTOutput.data.choices[0].text);        // return GPTOutput['choices'][0]['text']
+        return GPTOutput.data.choices[0].text
+    } catch (err) {
+        if (err.response) {
+            console.log(err.response.status);
+            console.log(err.response.data);
+        } else {
+            console.log(err.message);
+        }
+    }
+}
+
+module.exports = {APIcall, singleGPTCall };
